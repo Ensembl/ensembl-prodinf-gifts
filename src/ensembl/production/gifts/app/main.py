2033e02d70445d9890ea76bdc200ab2e6cb9b1d5
James Allen <jallen@ebi.ac.uk>
2021-03-18 10:37:13 +0000

    Removed 'publish_mapping' endpoint, didn't need to develop that, as it turned out. Changed paths to reflect new directory structure. Made hive db local, because we need to switch between two hive dbs, so don't want the wrong one to persist.

--- src/ensembl/production/gifts/app/main.py
+++ src/ensembl/production/gifts/app/main.py
@@ -1,16 +1,15 @@
 import os
 import requests
-from flask import Flask, flash, json, jsonify, redirect, render_template, request, url_for
+from flask import Flask, json, jsonify, redirect, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 from flasgger import Swagger
 
-from ensembl.gifts.config import GIFTsConfig
-from ensembl.forms import GIFTsSubmissionForm
-from ensembl.hive import HiveInstance
+from ensembl.production.gifts.config import GIFTsConfig
+from ensembl.production.gifts.forms import GIFTsSubmissionForm
+from ensembl.production.core.models.hive import HiveInstance
 
-# Go up two levels to get to root, where we will find the static and template files
-app_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+app_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 static_path = os.path.join(app_path, 'static')
 template_path = os.path.join(app_path, 'templates')
 
@@ -23,8 +22,6 @@ Bootstrap(app)
 CORS(app)
 
 Swagger(app, template_file='swagger.yml')
-
-hive = None
 
 
 def get_gifts_api_uri(environment):
@@ -54,25 +51,18 @@ def get_status(rest_server):
 
 
 def get_hive(hive_type):
-    global hive
-    if hive is None:
-        if hive_type == 'update_ensembl':
-            if app.config["HIVE_UPDATE_ENSEMBL_URI"] is None:
-                raise RuntimeError('Undefined environment variable: HIVE_UPDATE_ENSEMBL_URI')
-            else:
-                hive = HiveInstance(app.config["HIVE_UPDATE_ENSEMBL_URI"])
-        elif hive_type == 'process_mapping':
-            if app.config["HIVE_PROCESS_MAPPING_URI"] is None:
-                raise RuntimeError('Undefined environment variable: HIVE_PROCESS_MAPPING_URI')
-            else:
-                hive = HiveInstance(app.config["HIVE_PROCESS_MAPPING_URI"])
-        elif hive_type == 'publish_mapping':
-            if app.config["HIVE_PUBLISH_MAPPING_URI"] is None:
-                raise RuntimeError('Undefined environment variable: HIVE_PUBLISH_MAPPING_URI')
-            else:
-                hive = HiveInstance(app.config["HIVE_PUBLISH_MAPPING_URI"])
+    if hive_type == 'update_ensembl':
+        if app.config["HIVE_UPDATE_ENSEMBL_URI"] is None:
+            raise RuntimeError('Undefined environment variable: HIVE_UPDATE_ENSEMBL_URI')
         else:
-            raise RuntimeError('Unrecognised Pipeline: %s' % hive_type)
+            hive = HiveInstance(app.config["HIVE_UPDATE_ENSEMBL_URI"])
+    elif hive_type == 'process_mapping':
+        if app.config["HIVE_PROCESS_MAPPING_URI"] is None:
+            raise RuntimeError('Undefined environment variable: HIVE_PROCESS_MAPPING_URI')
+        else:
+            hive = HiveInstance(app.config["HIVE_PROCESS_MAPPING_URI"])
+    else:
+        raise RuntimeError('Unrecognised Pipeline: %s' % hive_type)
     return hive
 
 
@@ -102,7 +92,7 @@ def submit_job(payload, analysis, action):
 
 @app.route('/gifts/', methods=['GET'])
 def index():
-    return render_template('ensembl/gifts/index.html')
+    return render_template('index.html')
 
 
 @app.route('/gifts/update_ensembl/', methods=['POST'])
@@ -119,16 +109,17 @@ def update_ensembl_list():
     if request.is_json:
         return jsonify(jobs)
     else:
-        return render_template('ensembl/gifts/list.html', submission_type='Update Ensembl', jobs=jobs)
+        return render_template('list.html', submission_type='Update Ensembl', jobs=jobs)
 
 
 @app.route('/gifts/update_ensembl/<int:job_id>', methods=['GET'])
 def update_ensembl_result(job_id):
     job = get_hive('update_ensembl').get_result_for_job_id(job_id, progress=False)
+
     if request.is_json:
         return jsonify(job)
     else:
-        return render_template('ensembl/gifts/detail.html', submission_type='Update Ensembl', job=job)
+        return render_template('list.html', submission_type='Update Ensembl', jobs=[job])
 
 
 @app.route('/gifts/process_mapping/', methods=['POST'])
@@ -145,7 +136,7 @@ def process_mapping_list():
     if request.is_json:
         return jsonify(jobs)
     else:
-        return render_template('ensembl/gifts/list.html', submission_type='Process Mapping', jobs=jobs)
+        return render_template('list.html', submission_type='Process Mapping', jobs=jobs)
 
 
 @app.route('/gifts/process_mapping/<int:job_id>', methods=['GET'])
@@ -155,34 +146,7 @@ def process_mapping_result(job_id):
     if request.is_json:
         return jsonify(job)
     else:
-        return render_template('ensembl/gifts/detail.html', submission_type='Process Mapping', job=job)
-
-
-@app.route('/gifts/publish_mapping/', methods=['POST'])
-def publish_mapping(payload=None):
-    analysis = app.config['HIVE_PUBLISH_MAPPING_ANALYSIS']
-    return submit_job(payload, analysis, 'publish_mapping')
-
-
-@app.route('/gifts/publish_mapping/', methods=['GET'])
-def publish_mapping_list():
-    analysis = app.config['HIVE_PUBLISH_MAPPING_ANALYSIS']
-    jobs = get_hive('publish_mapping').get_all_results(analysis)
-
-    if request.is_json:
-        return jsonify(jobs)
-    else:
-        return render_template('ensembl/gifts/list.html', submission_type='Publish Mapping', jobs=jobs)
-
-
-@app.route('/gifts/publish_mapping/<int:job_id>', methods=['GET'])
-def publish_mapping_result(job_id):
-    job = get_hive('publish_mapping').get_result_for_job_id(job_id, progress=False)
-
-    if request.is_json:
-        return jsonify(job)
-    else:
-        return render_template('ensembl/gifts/detail.html', submission_type='Publish Mapping', job=job)
+        return render_template('list.html', submission_type='Process Mapping', jobs=[job])
 
 
 @app.route('/gifts/submit/', methods=['GET'])
@@ -190,7 +154,7 @@ def display_form(status=None):
     form = GIFTsSubmissionForm(request.form)
 
     return render_template(
-        'ensembl/gifts/submit.html',
+        'submit.html',
         form=form,
         status=status
     )
@@ -214,8 +178,6 @@ def submit_form():
         return update_ensembl(payload)
     elif form.process_mapping.data:
         return process_mapping(payload)
-    elif form.publish_mapping.data:
-        return publish_mapping(payload)
     else:
         raise RuntimeError('Unrecognised submission type')
 
